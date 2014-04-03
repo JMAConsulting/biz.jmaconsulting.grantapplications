00802d27f27256a260b17b40f1f9f3765d54590c
Edsel <edsel.lopez@jmaconsulting.biz>
2014-04-03 20:51:32 +0530

    GEP-35 Additional work on grantapplications

--- grantapplications.php
+++ grantapplications.php
@@ -86,6 +86,39 @@ function grantapplications_civicrm_managed(&$entities) {
 
 function grantapplications_civicrm_validate($formName, &$fields, &$files, &$form) {
   $errors = array();
+  if ($formName == 'CRM_Grant_Form_Grant_Main' && isset($fields['grant_id'])) {
+    $grantType = CRM_Core_DAO::getFieldValue("CRM_Grant_DAO_Grant", $fields['grant_id'], "grant_type_id");
+    $groupTree = &CRM_Core_BAO_CustomGroup::getTree("Grant", $this, $fields['grant_id'], 0, $grantType);
+    foreach ($groupTree as $field => $value) {
+      if (isset($value['fields'])) {
+        foreach ($value['fields'] as $key => $f) {
+          if (CRM_Utils_Array::value('html_type', $f) == 'File' && isset($f['customValue'][1]['fid'])) {
+            $form->setElementError('custom_'.$key, NULL);
+          }
+        }
+      }
+    }
+    // On Behalf
+    $ssParams['id'] = CRM_Core_DAO::singleValueQuery('SELECT id FROM civicrm_saved_search WHERE form_values LIKE "%\"grant_id\";i:'.$fields['grant_id'].'%"');
+    CRM_Contact_BAO_SavedSearch::retrieve($ssParams, $savedSearch);
+    $grantParams = unserialize($savedSearch['form_values']);
+    $subType = CRM_Contact_BAO_ContactType::subTypeInfo('Organization', TRUE);
+    foreach ($subType as $key => $value) {
+      $gTree[] = &CRM_Core_BAO_CustomGroup::getTree("Organization", $this, $grantParams['contactID'], NULL, $key);
+    }
+    foreach ($gTree as $flds => $vs) {
+      foreach ($vs as $fld => $v) {
+        if (isset($v['fields'])) {
+          foreach ($v['fields'] as $k => $f) {
+            if (CRM_Utils_Array::value('html_type', $f) == 'File' && isset($f['customValue'][1]['fid'])) {
+              $form->_errors['onbehalf[custom_'.$k.']'] = '';
+              $form->setElementError('onbehalf[custom_'.$k.']', NULL);
+            }
+          }
+        }
+      }
+    }
+  }
   if (($formName == 'CRM_Grant_Form_Grant_Main' ||  $formName == 'CRM_Grant_Form_Grant_Confirm') 
     && $form->_values['is_draft'] == 1 && (CRM_Utils_Array::value('_qf_Main_save', $fields) == 'Save as Draft' || $form->_params['is_draft'] == 1)) {
     foreach($form->_fields as $name => $values) {
