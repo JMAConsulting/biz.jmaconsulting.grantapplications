2c727499ee1db41e028f0c1728aa0a2334690644
Pradeep Nayak <pradpnayak@gmail.com>
2014-12-25 01:21:24 +0530

    -- fixed upgrade, GEP-50

--- sql/upgrade_4301.sql
+++ sql/upgrade_4301.sql
@@ -1,7 +1,3 @@
-ALTER TABLE  `civicrm_grant_app_page` ADD `is_for_organization` TINYINT( 4 ) NULL DEFAULT  '0' COMMENT 'if true, signup is done on behalf of an organization';
-
-ALTER TABLE  `civicrm_grant_app_page` ADD `for_organization` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT 'This text field is shown when is_for_organization is checked. For example - I am submitting grant application on behalf of an organization.';
-
 ALTER TABLE  `civicrm_grant_app_page` ADD `is_draft` TINYINT(4) DEFAULT NULL COMMENT 'Does this page have a Save as Draft button?' AFTER `default_amount`;
 
 ALTER TABLE  `civicrm_grant_app_page` ADD `draft_title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Title for Save as Draft page (header title tag, and display at the top of the page).' AFTER `is_draft`;
@@ -18,7 +14,7 @@ SELECT @maxValue := MAX( CAST( `value` AS UNSIGNED ) ) + 1 FROM  `civicrm_option
 
 SELECT @maxWeight := MAX( CAST( `weight` AS UNSIGNED ) ) + 1 FROM  `civicrm_option_value` WHERE `option_group_id` = @dashId;
 
-INSERT IGNORE INTO `civicrm_option_value` (`option_group_id`, {localize field='label'}`label`{/localize}, `value`, `name`, `weight`, {localize field='description'}`description`{/localize}, `is_active`) VALUES (@dashId, {localize}'{ts escape="sql"}Grants{/ts}'{/localize}, @maxValue, 'CiviGrant', @maxWeight, {localize}'Grants on dashboard'{/localize}, 1);
+INSERT IGNORE INTO `civicrm_option_value` (`option_group_id`, `label`, `value`, `name`, `weight`, `description`, `is_active`) VALUES (@dashId, 'Grants', @maxValue, 'CiviGrant', @maxWeight, 'Grants on dashboard', 1);
 
 SELECT @statusId := id FROM `civicrm_option_group` WHERE `name` = 'grant_status';
 
@@ -26,5 +22,10 @@ SELECT @maxValue := MAX( CAST( `value` AS UNSIGNED ) ) + 1 FROM  `civicrm_option
 
 SELECT @maxWeight := MAX( CAST( `weight` AS UNSIGNED ) ) + 1 FROM  `civicrm_option_value` WHERE `option_group_id` = @statusId;
 
-INSERT IGNORE INTO `civicrm_option_value` (`option_group_id`, {localize field='label'}`label`{/localize}, `value`, `name`, `weight`, `is_active`) VALUES (@statusId, {localize}'{ts escape="sql"}Draft{/ts}'{/localize}, @maxValue, 'Draft', @maxWeight, 1);
+INSERT IGNORE INTO `civicrm_option_value` (`option_group_id`, `label`, `value`, `name`, `weight`, `is_active`) VALUES (@statusId, 'Draft', @maxValue, 'Draft', @maxWeight, 1);
 
+DELETE FROM civicrm_uf_field WHERE field_name = 'grant_application_received_date';
+
+UPDATE civicrm_uf_field 
+SET  field_name = 'amount_requested'
+WHERE field_name = 'grant_amount_requested';
