81531355e5fe5a0a99a87d7270a6c139971a627e
Edsel <edsel.lopez@jmaconsulting.biz>
2017-08-01 16:23:30 +0530

    GEP-95 Updated Civix file

--- grantapplications.civix.php
+++ grantapplications.civix.php
@@ -3,38 +3,34 @@
 // AUTO-GENERATED FILE -- Civix may overwrite any changes made to this file
 
 /**
- * (Delegated) Implements hook_civicrm_config().
+ * (Delegated) Implementation of hook_civicrm_config
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
  */
 function _grantapplications_civix_civicrm_config(&$config = NULL) {
   static $configured = FALSE;
-  if ($configured) {
-    return;
-  }
+  if ($configured) return;
   $configured = TRUE;
 
   $template =& CRM_Core_Smarty::singleton();
 
-  $extRoot = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+  $extRoot = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
   $extDir = $extRoot . 'templates';
 
   if ( is_array( $template->template_dir ) ) {
       array_unshift( $template->template_dir, $extDir );
-  }
-  else {
+  } else {
       $template->template_dir = array( $extDir, $template->template_dir );
   }
 
   $include_path = $extRoot . PATH_SEPARATOR . get_include_path( );
-  set_include_path($include_path);
+  set_include_path( $include_path );
 }
 
 /**
- * (Delegated) Implements hook_civicrm_xmlMenu().
+ * (Delegated) Implementation of hook_civicrm_xmlMenu
  *
  * @param $files array(string)
- *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
  */
 function _grantapplications_civix_civicrm_xmlMenu(&$files) {
@@ -44,7 +40,7 @@ function _grantapplications_civix_civicrm_xmlMenu(&$files) {
 }
 
 /**
- * Implements hook_civicrm_install().
+ * Implementation of hook_civicrm_install
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
@@ -56,7 +52,7 @@ function _grantapplications_civix_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
+ * Implementation of hook_civicrm_uninstall
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
  */
@@ -68,7 +64,7 @@ function _grantapplications_civix_civicrm_uninstall() {
 }
 
 /**
- * (Delegated) Implements hook_civicrm_enable().
+ * (Delegated) Implementation of hook_civicrm_enable
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
@@ -82,7 +78,7 @@ function _grantapplications_civix_civicrm_enable() {
 }
 
 /**
- * (Delegated) Implements hook_civicrm_disable().
+ * (Delegated) Implementation of hook_civicrm_disable
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
  * @return mixed
@@ -97,7 +93,7 @@ function _grantapplications_civix_civicrm_disable() {
 }
 
 /**
- * (Delegated) Implements hook_civicrm_upgrade().
+ * (Delegated) Implementation of hook_civicrm_upgrade
  *
  * @param $op string, the type of operation being performed; 'check' or 'enqueue'
  * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
@@ -119,8 +115,7 @@ function _grantapplications_civix_civicrm_upgrade($op, CRM_Queue_Queue $queue = 
 function _grantapplications_civix_upgrader() {
   if (!file_exists(__DIR__.'/CRM/Grantapplications/Upgrader.php')) {
     return NULL;
-  }
-  else {
+  } else {
     return CRM_Grantapplications_Upgrader_Base::instance();
   }
 }
@@ -163,7 +158,7 @@ function _grantapplications_civix_find_files($dir, $pattern) {
   return $result;
 }
 /**
- * (Delegated) Implements hook_civicrm_managed().
+ * (Delegated) Implementation of hook_civicrm_managed
  *
  * Find any *.mgd.php files, merge their content, and return.
  *
@@ -183,7 +178,7 @@ function _grantapplications_civix_civicrm_managed(&$entities) {
 }
 
 /**
- * (Delegated) Implements hook_civicrm_caseTypes().
+ * (Delegated) Implementation of hook_civicrm_caseTypes
  *
  * Find any and return any files matching "xml/case/*.xml"
  *
@@ -212,31 +207,6 @@ function _grantapplications_civix_civicrm_caseTypes(&$caseTypes) {
 }
 
 /**
- * (Delegated) Implements hook_civicrm_angularModules().
- *
- * Find any and return any files matching "ang/*.ang.php"
- *
- * Note: This hook only runs in CiviCRM 4.5+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function _grantapplications_civix_civicrm_angularModules(&$angularModules) {
-  if (!is_dir(__DIR__ . '/ang')) {
-    return;
-  }
-
-  $files = _grantapplications_civix_glob(__DIR__ . '/ang/*.ang.php');
-  foreach ($files as $file) {
-    $name = preg_replace(':\.ang\.php$:', '', basename($file));
-    $module = include $file;
-    if (empty($module['ext'])) {
-      $module['ext'] = 'biz.jmaconsulting.grantapplications';
-    }
-    $angularModules[$name] = $module;
-  }
-}
-
-/**
  * Glob wrapper which is guaranteed to return an array.
  *
  * The documentation for glob() says, "On some systems it is impossible to
@@ -254,24 +224,30 @@ function _grantapplications_civix_glob($pattern) {
 }
 
 /**
- * Inserts a navigation menu item at a given place in the hierarchy.
+ * Inserts a navigation menu item at a given place in the hierarchy
  *
- * @param array $menu - menu hierarchy
- * @param string $path - path where insertion should happen (ie. Administer/System Settings)
- * @param array $item - menu you need to insert (parent/child attributes will be filled for you)
+ * $menu - menu hierarchy
+ * $path - path where insertion should happen (ie. Administer/System Settings)
+ * $item - menu you need to insert (parent/child attributes will be filled for you)
+ * $parentId - used internally to recurse in the menu structure
  */
-function _grantapplications_civix_insert_navigation_menu(&$menu, $path, $item) {
+function _grantapplications_civix_insert_navigation_menu(&$menu, $path, $item, $parentId = NULL) {
+  static $navId;
+
   // If we are done going down the path, insert menu
   if (empty($path)) {
-    $menu[] = array(
-      'attributes' => array_merge(array(
+    if (!$navId) $navId = CRM_Core_DAO::singleValueQuery("SELECT max(id) FROM civicrm_navigation");
+    $navId ++;
+    $menu[$navId] = array (
+      'attributes' => array_merge($item, array(
         'label'      => CRM_Utils_Array::value('name', $item),
         'active'     => 1,
-      ), $item),
+        'parentID'   => $parentId,
+        'navID'      => $navId,
+      ))
     );
-    return TRUE;
-  }
-  else {
+    return true;
+  } else {
     // Find an recurse into the next level down
     $found = false;
     $path = explode('/', $path);
@@ -287,58 +263,13 @@ function _grantapplications_civix_insert_navigation_menu(&$menu, $path, $item) {
 }
 
 /**
- * (Delegated) Implements hook_civicrm_navigationMenu().
- */
-function _grantapplications_civix_navigationMenu(&$nodes) {
-  if (!is_callable(array('CRM_Core_BAO_Navigation', 'fixNavigationMenu'))) {
-    _grantapplications_civix_fixNavigationMenu($nodes);
-  }
-}
-
-/**
- * Given a navigation menu, generate navIDs for any items which are
- * missing them.
- */
-function _grantapplications_civix_fixNavigationMenu(&$nodes) {
-  $maxNavID = 1;
-  array_walk_recursive($nodes, function($item, $key) use (&$maxNavID) {
-    if ($key === 'navID') {
-      $maxNavID = max($maxNavID, $item);
-    }
-    });
-  _grantapplications_civix_fixNavigationMenuItems($nodes, $maxNavID, NULL);
-}
-
-function _grantapplications_civix_fixNavigationMenuItems(&$nodes, &$maxNavID, $parentID) {
-  $origKeys = array_keys($nodes);
-  foreach ($origKeys as $origKey) {
-    if (!isset($nodes[$origKey]['attributes']['parentID']) && $parentID !== NULL) {
-      $nodes[$origKey]['attributes']['parentID'] = $parentID;
-    }
-    // If no navID, then assign navID and fix key.
-    if (!isset($nodes[$origKey]['attributes']['navID'])) {
-      $newKey = ++$maxNavID;
-      $nodes[$origKey]['attributes']['navID'] = $newKey;
-      $nodes[$newKey] = $nodes[$origKey];
-      unset($nodes[$origKey]);
-      $origKey = $newKey;
-    }
-    if (isset($nodes[$origKey]['child']) && is_array($nodes[$origKey]['child'])) {
-      _grantapplications_civix_fixNavigationMenuItems($nodes[$origKey]['child'], $maxNavID, $nodes[$origKey]['attributes']['navID']);
-    }
-  }
-}
-
-/**
- * (Delegated) Implements hook_civicrm_alterSettingsFolders().
+ * (Delegated) Implementation of hook_civicrm_alterSettingsFolders
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
  */
 function _grantapplications_civix_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   static $configured = FALSE;
-  if ($configured) {
-    return;
-  }
+  if ($configured) return;
   $configured = TRUE;
 
   $settingsDir = __DIR__ . DIRECTORY_SEPARATOR . 'settings';
