c33b14dbceeffd664466259977e7b5cfccd3afc9
Jamie McClelland <jm@mayfirst.org>
2018-09-12 15:41:28 -0400

    ensure on behalf of organization works properly.
    See https://github.com/JMAConsulting/biz.jmaconsulting.grantapplications/issues/140

--- CRM/Grant/Form/Grant/Confirm.php
+++ CRM/Grant/Form/Grant/Confirm.php
@@ -519,7 +519,7 @@ class CRM_Grant_Form_Grant_Confirm extends CRM_Grant_Form_GrantBase {
     }
     // If onbehalf-of-organization grant application add organization
     // and it's location.
-    if (isset($this->_values['onbehalf_profile_id']) && isset($behalfOrganization['organization_name']) && !empty($this->_params['is_for_organization'])) {
+    if (isset($this->_values['onbehalf_profile_id']) && isset($behalfOrganization['organization_name'])) {
       $ufFields = array();
       foreach ($this->_fields['onbehalf'] as $name => $value) {
         $ufFields[$name] = 1;
