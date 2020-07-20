621fdf09a62e843e2d565a8dee3eec67602815df
Seamus Lee <seamuslee001@gmail.com>
2020-07-20 20:32:11 +1000

    Work on cleaning up and standardising the grant applications entity information

---
+++ xml/schema/CRM/Grantapplications/GrantApplicationPage.entityType.php
@@ -0,0 +1,10 @@
+<?php
+// This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
+// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+return [
+  [
+    'name' => 'GrantApplicationPage',
+    'class' => 'CRM_Grantapplications_DAO_GrantApplicationPage',
+    'table' => 'civicrm_grant_application_page',
+  ],
+];
