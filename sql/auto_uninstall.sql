76816175b3905301d2fa5652e40b0af90310ad04
Seamus Lee <seamuslee001@gmail.com>
2020-07-21 09:33:49 +1000

    Update xml to include comments as per original install file

--- sql/auto_uninstall.sql
+++ sql/auto_uninstall.sql
@@ -19,4 +19,28 @@ SET FOREIGN_KEY_CHECKS=0;
 
 DROP TABLE IF EXISTS `civicrm_grant_app_page`;
 
+DELETE  civicrm_option_value.*, civicrm_option_group.*, civicrm_msg_template.*
+FROM civicrm_option_value
+INNER JOIN civicrm_option_group ON  civicrm_option_value.option_group_id = civicrm_option_group.id
+INNER JOIN civicrm_msg_template ON civicrm_msg_template.workflow_id = civicrm_option_value.id
+WHERE civicrm_option_group.name LIKE 'msg_tpl_workflow_grant';
+
+DELETE ca.*, cv.* FROM `civicrm_activity` ca
+INNER JOIN civicrm_option_value cv ON cv.value = ca.activity_type_id
+INNER JOIN civicrm_option_group cg ON cg.id = cv.option_group_id
+WHERE cg.name = 'activity_type' AND cv.name = 'Grant';
+
+
+DELETE ca.*, cv.* FROM `civicrm_activity` ca
+INNER JOIN civicrm_option_value cv ON cv.value = ca.activity_type_id
+INNER JOIN civicrm_option_group cg ON cg.id = cv.option_group_id
+WHERE cg.name = 'grant_status' AND cv.name = 'Draft';
+
+
+DELETE ca.*, cv.* FROM `civicrm_activity` ca
+INNER JOIN civicrm_option_value cv ON cv.value = ca.activity_type_id
+INNER JOIN civicrm_option_group cg ON cg.id = cv.option_group_id
+WHERE cg.name = 'user_dashboard_options' AND cv.name = 'CiviGrant';
+
+
 SET FOREIGN_KEY_CHECKS=1;
